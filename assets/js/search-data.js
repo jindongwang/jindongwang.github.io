6566b3c09d3e1029b9f713c94038364356034c5c
jindongwang <jindongwang@outlook.com>
2026-07-06 11:29:03 +0000

    Deploying to gh-pages from @ jindongwang/jindongwang.github.io@485dbb0b63616a200f222923dc39e202aed97d81 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -683,8 +683,8 @@ ninja.data = [{
           section: "News",},{id: "news-we-have-three-papers-accepted-at-iclr-2026-congrats",
           title: 'We have  three papers accepted at ICLR 2026! Congrats!',
           description: "",
-          section: "News",},{id: "news-i-received-the-aaai-2026-oustanding-spc-award-35-among-1700-spcs",
-          title: 'I received the AAAI 2026 Oustanding SPC Award (35 among 1700+ SPCs)!',
+          section: "News",},{id: "news-i-received-the-aaai-2026-outstanding-spc-award-35-among-1700-spcs",
+          title: 'I received the AAAI 2026 Outstanding SPC Award (35 among 1700+ SPCs)!',
           description: "",
           section: "News",},{id: "news-our-ai-education-paper-classroom-ai-large-language-models-as-grade-specific-teachers-has-been-accepted-by-npj-artificial-intelligence-a-nature-portfolio-journal",
           title: 'Our AI education paper “Classroom AI: Large Language Models as Grade-Specific Teachers” has...',
@@ -713,49 +713,4 @@ ninja.data = [{
           section: "News",},{id: "news-one-paper-is-accepted-by-eccv-2026-congrats",
           title: 'One paper is accepted by ECCV 2026. Congrats!',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},];
+          section: "News",},];
